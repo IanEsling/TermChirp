e614ec0a0d8bed29db91d4746681ff12b5b69d7f
Ian <ian.esling@gmail.com>
2015-04-16 00:22:51 +0100

    bit o' fyodor

--- src/main/java/ConsoleReader.java
+++ src/main/java/ConsoleReader.java
@@ -1,14 +1,17 @@
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class ConsoleReader {
 
-    public ConsoleReader(Scanner scanner, CommandInterpreter commandInterpreter) {
+    public ConsoleReader(InputStream input, PrintStream output, CommandInterpreter commandInterpreter) {
+        Scanner scanner = new Scanner(input);
         String[] command = new String[3];
         int i = 0;
        while (scanner.hasNext()) {
            command[i] = scanner.next();
            i++;
        }
-        commandInterpreter.command(command);
+        output.println(commandInterpreter.command(command));
     }
 }
--- src/test/java/ConsoleTweetTest.java
+++ src/test/java/ConsoleTweetTest.java
@@ -2,25 +2,36 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import uk.org.fyodor.generators.Generator;
+import uk.org.fyodor.generators.RDG;
 
-import java.io.StringReader;
-import java.util.Scanner;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConsoleTweetTest {
 
-    Scanner scanner;
+    Generator<Command> randomCommand = RDG.value(Command.values());
+
     @Mock
     CommandInterpreter commandInterpreter;
+    @Mock
+    PrintStream output;
 
     @Test
-    public void getCommands(){
-        StringReader source = new StringReader("string0 string1 string2");
-        scanner = new Scanner(source);
-        ConsoleReader testee = new ConsoleReader(scanner, commandInterpreter);
+    public void getCommands() {
+        Command command = randomCommand.next();
+        given(commandInterpreter.command(anyString(), anyString(), anyString()))
+                .willReturn(command);
+        InputStream input = new ByteArrayInputStream("string0 string1 string2".getBytes());
+        new ConsoleReader(input, output, commandInterpreter);
         verify(commandInterpreter).command("string0", "string1", "string2");
+        verify(output).println(command);
     }
 
 }
